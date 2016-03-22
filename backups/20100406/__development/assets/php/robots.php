ef9482328c05ade67b3ceff403a3a238059425f5
Jip Moors <jip@yoast.com>
2016-03-22 20:59:34 +0100

    Cleaned up old backups

--- backups/20100406/__development/assets/php/robots.php
+++
@@ -1,9 +0,0 @@
-/*
-<?php
-session_start();
-$forceSingle = (intval($_SESSION['user_id']) == 0)?1:0;
-
-?>
-*/
-var PHPSESSION = '<?= session_id() ?>';
-var forceSingle = parseInt('<?= $forceSingle ?>');
--- backups/20100406/__development/logs/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Redirector!
-header("Location: ../");
-
-?>
